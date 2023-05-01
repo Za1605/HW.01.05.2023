2edb4e4555ef358c5c9f201f96798ed1b6c14c4e
YuliiaT <za1605@ukr.net>
2023-05-01 23:11:14 +0300

    first commi

---
+++ 05.js
@@ -0,0 +1,74 @@
+
+let a=`hello`
+
+console.log(a);
+
+let b=`owu`
+
+console.log(b);
+
+let c=`com`
+
+console.log(c);
+
+let d=`ua`
+
+console.log(d);
+
+let e=`1`
+
+console.log(e);
+
+let f=`10`
+
+console.log(f);
+
+let g=`-999`
+
+console.log(g);
+
+let i=`123`
+
+console.log(i);
+
+let h=`3.14`
+
+console.log(h);
+
+let j=`2.7`
+
+console.log(j);
+
+let k=`16`
+
+console.log(k);
+
+let l=`true`
+
+console.log(l);
+
+let m=`false`
+
+console.log(m);
+
+
+
+let firstName =`Yuliia`
+let lastName = `Timcenko`
+let middleName = `Vasylyvna`
+
+let person = firstName+ ` `+ middleName+ ` `+lastName
+
+console.log(person);
+
+let r = 100;
+console.log(typeof (r));
+
+let s = `100`;
+
+console.log(typeof (s));
+
+let t = true;
+console.log(typeof (t));
+
+
